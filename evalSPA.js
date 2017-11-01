8e0d699145f52dccf0612edb444f8e8e96ff9835
rootsofworld <faith74731@hotmail.com.tw>
2017-11-02 04:49:24 +0800

    Make some progress

--- evalSPA.js
+++ evalSPA.js
@@ -44,13 +44,23 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 //const fbgraph = require('fbgraph');
 const moment = require('moment');
-const request = require('request')
+//const request = require('request')
 const chalk = require('chalk')
+const nodejieba = require('nodejieba')
 const appInfo = require('./app_config.json');
-const sample = require('./sample.json')
+//const sample = require('./sample.json')
 
 
 function evalSPA(option, appInfo) {
+
+    let result = {}
+    
+    let postsTypeCount = {
+        video : 0,
+        photo : 0,
+        link : 0
+    }
+
 
     function generateUrlWithoutAccesstoken( option, appInfo ){
 
@@ -58,10 +68,10 @@ function evalSPA(option, appInfo) {
         let until = moment(option.until, ['YYYY-MM-DD','YYYY-MM-DD HH:mm']).valueOf() / 1000
 
         return `
-        https://graph.facebook.com/v2.10/${option.target}?access_token=${appInfo.appId}|${appInfo.appSecret}&fields=id,posts.limit(${option.limit}).since(${since}).until(${until}){message,link,message_tags,permalink_url,picture,story,target,type,source,timeline_visibility,status_type,description,caption,created_time}
+        https://graph.facebook.com/v2.10/${option.target}?access_token=${appInfo.appId}|${appInfo.appSecret}&fields=id,name,posts.limit(${option.limit}).since(${since}).until(${until}){type,created_time,message,link,message_tags,permalink_url,picture,description,caption}
         `
     }
-
+   
 
     const graphUrl = generateUrlWithoutAccesstoken( option, appInfo )
     //console.log(graphUrl)
@@ -73,30 +83,71 @@ function evalSPA(option, appInfo) {
         console.log(JSON.stringify(JSON.parse(res.body)));
     })*/
 
+    function modifyPostData(post){
+        
+        post.sourceArticle = {
+            raw : "",
+            words : []
+        }
+        post.messageWords = []
+        return post
+    }
+
+    result.data = []
     rp( graphUrl )
         .then( ( res ) => {
+
             res = JSON.parse(res);
-            let linkPosts = res.posts.data.filter((post) => {
-                    return post.type === "link";
-                })
-    ///////////////////Check if don't have link-type post////////////////
-            if(linkPosts.length === 0){ 
-                return false
-            }else{
-                //console.log(linkPosts)
-                return rp(linkPosts[0].link)
-            }
+            result.page_name = res.name
+            result.page_id = res.id
+            result.since = option.since
+            result.until = option.until
+
+            res.posts.data.forEach((post) => {
+                if(post.type === 'video'){
+
+                    postsTypeCount.video++
+
+                }else if(post.type === 'photo'){
+
+                    postsTypeCount.photo++
+
+                }else if(post.type === 'link'){
+
+                    postsTypeCount.link++
+                    result.data.push(modifyPostData(post))
+
+                }else{
+
+                    console.log(chalk.red("[Warning] There is some unknown post type: " + post.type))
+                }
+            })
+            
+            console.log(chalk.yellow("TypeCounter: "))
+            console.log(chalk.yellowBright(`\tVideo : ${postsTypeCount.video}`))
+            console.log(chalk.yellowBright(`\tPhoto : ${postsTypeCount.photo}`))
+            console.log(chalk.yellowBright(`\tLink : ${postsTypeCount.link}`))
+
+            console.log(result)
+            console.log(result.data.length)
+            
+            return result
+
         })
-        .then(( source ) => {
-            if(!source){
+        .then(( result ) => {
+            if(result.data.length < 1){
                 console.log("Don't have link-type post!!")
             }else{
                 //console.log(source);
-                let articleContent = extractArticle(source)
-                /*fs.writeFile('test.html', source, 'utf8', (err) => {
-                    if(err) throw err;
-                })*/
-                fs.writeFile('test.txt', articleContent, 'utf8', (err) => {
+                result.data.map((post) => {
+
+                    post.sourceArticle.raw = extractArticle(post.link)
+                    post.sourceArticle.words = nodejieba.extract(post.sourceArticle.raw,5)
+                    post.messageWords = nodejieba.extract(post.message,5)
+
+                })
+                
+                fs.writeFile('result.json', JSON.stringify(result), 'utf8', (err) => {
                     if(err) throw err;
                 })
             }
@@ -110,19 +161,42 @@ function evalSPA(option, appInfo) {
      * @param {String} className //main article section's className
      * @param {Function} callback  //When extraction is done
      */
-    function extractArticle( body ){
-        
-        let pElementCatcher = /<p>.*(?=<\/p>)/g;
-        $ = cheerio.load(body)
-        let pList = $('p')
-        articleContent = pElementCatcher.exec(body)
-        //let mainSection = $(`.${className}`)
-        console.log(pList.length)
-        for(let i = 0; i < pList.length; i++){
-            if(i % 2 === 0)
-                console.log(chalk.yellowBright(pList[i].children[0].data))
-                console.log(chalk.redBright(pList[i].children[0].data))
-        }
-        return articleContent
+    function extractArticle( url ){
+        let pList
+         rp(url)
+            .then((body) => {
+
+                $ = cheerio.load(body)
+                pList = $('p')
+                
+                //let mainSection = $(`.${className}`)
+                console.log(pList.length)
+                for(let i = 0; i < pList.length; i++){
+                    if(i % 2 === 0)
+                        console.log(chalk.yellowBright(pList[i].children[0].data))
+                        console.log(chalk.redBright(pList[i].children[0].data))
+                }
+                return pList
+            })
+        return pList
     }
 }
+
+function punctuactionFilter(str){
+    
+}
+
+function stopwordFilter(str){
+    
+}
+
+
+
+evalSPA({
+    target: "466505160192708",
+    since: "2017-10-01",
+    until: "2017-10-07",
+    limit: 100
+},appInfo)
+
+module.exports = evalSPA
